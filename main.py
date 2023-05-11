4c8816790b7cc1ad4e788b73dff21496715fbf1a
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-05-11 14:41:18 +0300

    correct display of years since foundation

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from datetime import datetime, date
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+years_since_foundation = date.today().year - datetime(year=1920, month=1, day=1).year
+rendered_page = template.render(years_since_foundation=years_since_foundation)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
