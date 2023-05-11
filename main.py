47a178a41ee1bf1b47ab8c93885eed1306eae180
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-05-11 23:13:55 +0300

    correct the display of the year word

--- main.py
+++ main.py
@@ -3,14 +3,30 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import datetime, date
 
+
+def define_year_word(years_since_foundation):
+    if 10 <= (years_since_foundation % 100) <= 19 or not years_since_foundation % 10:
+        year_word = 'лет'
+    elif 5 <= years_since_foundation % 10 <= 9:
+        year_word = 'лет'
+    elif years_since_foundation % 10 == 1:
+        year_word = 'год'
+    else:
+        year_word = 'года'
+    return year_word
+
+
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
 )
 
 template = env.get_template('template.html')
+
 years_since_foundation = date.today().year - datetime(year=1920, month=1, day=1).year
-rendered_page = template.render(years_since_foundation=years_since_foundation)
+
+rendered_page = template.render(years_since_foundation=years_since_foundation,
+                                year_word=define_year_word(years_since_foundation))
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
