bf4265f5b774b2948c0ac69453d62536ad0fd614
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-05-12 16:58:00 +0300

    step 13 complete

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import datetime, date
+import pandas
+from pprint import pprint
 
 
 def define_year_word(years_since_foundation):
@@ -25,8 +27,24 @@ template = env.get_template('template.html')
 
 years_since_foundation = date.today().year - datetime(year=1920, month=1, day=1).year
 
+wines = pandas.read_excel('wine.xlsx',
+                          na_values=['N/A', 'NA'], keep_default_na=False)
+wines = wines.to_dict(orient='records')
+
+wines2 = pandas.read_excel('wine2.xlsx',
+                          na_values=['N/A', 'NA'], keep_default_na=False)
+wines2 = wines2.to_dict(orient='records')
+
+drinks = {}
+for drink in wines2:
+    drinks[drink['Категория']] = []
+for drink in drinks:
+    drinks[drink] = [wine for wine in wines2 if wine['Категория'] == drink]
+pprint(drinks)
+
 rendered_page = template.render(years_since_foundation=years_since_foundation,
-                                year_word=define_year_word(years_since_foundation))
+                                year_word=define_year_word(years_since_foundation),
+                                wines=wines)
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
