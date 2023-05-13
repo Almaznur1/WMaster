3794fbbd1b5cf54828395761ea55c3a1395a37cb
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-05-13 18:36:13 +0300

    divide drinks into different categories

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import datetime, date
 import pandas
 from pprint import pprint
+import collections
 
 
 def define_year_word(years_since_foundation):
@@ -27,24 +28,19 @@ template = env.get_template('template.html')
 
 years_since_foundation = date.today().year - datetime(year=1920, month=1, day=1).year
 
-wines = pandas.read_excel('wine.xlsx',
+wines = pandas.read_excel('wine2.xlsx',
                           na_values=['N/A', 'NA'], keep_default_na=False)
 wines = wines.to_dict(orient='records')
 
-wines2 = pandas.read_excel('wine2.xlsx',
-                          na_values=['N/A', 'NA'], keep_default_na=False)
-wines2 = wines2.to_dict(orient='records')
-
-drinks = {}
-for drink in wines2:
+drinks = collections.defaultdict(list)
+for drink in wines:
     drinks[drink['Категория']] = []
 for drink in drinks:
-    drinks[drink] = [wine for wine in wines2 if wine['Категория'] == drink]
-pprint(drinks)
+    drinks[drink] = [wine for wine in wines if wine['Категория'] == drink]
 
 rendered_page = template.render(years_since_foundation=years_since_foundation,
                                 year_word=define_year_word(years_since_foundation),
-                                wines=wines)
+                                drinks=drinks)
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
