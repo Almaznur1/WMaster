84f3eb2a6f62f6640ecf4206dfe6b77c3285d480
AlmazNuriakhmetov <aboohnifa@gmail.com>
2023-05-15 09:53:51 +0300

    added def main() wrapper

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import datetime, date
 import pandas
-from pprint import pprint
 import collections
 
 
@@ -19,31 +18,36 @@ def define_year_word(years_since_foundation):
     return year_word
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-template = env.get_template('template.html')
+    template = env.get_template('template.html')
 
-years_since_foundation = date.today().year - datetime(year=1920, month=1, day=1).year
+    years_since_foundation = date.today().year - datetime(year=1920, month=1, day=1).year
 
-wines = pandas.read_excel('wine3.xlsx',
-                          na_values=['N/A', 'NA'], keep_default_na=False)
-wines = wines.to_dict(orient='records')
+    wines = pandas.read_excel('wine3.xlsx',
+                              na_values=['N/A', 'NA'], keep_default_na=False)
+    wines = wines.to_dict(orient='records')
 
-drinks = collections.defaultdict(list)
-for drink in wines:
-    drinks[drink['Категория']] = []
-for drink in drinks:
-    drinks[drink] = [wine for wine in wines if wine['Категория'] == drink]
+    drinks = collections.defaultdict(list)
+    for drink in wines:
+        drinks[drink['Категория']] = []
+    for drink in drinks:
+        drinks[drink] = [wine for wine in wines if wine['Категория'] == drink]
 
-rendered_page = template.render(years_since_foundation=years_since_foundation,
-                                year_word=define_year_word(years_since_foundation),
-                                drinks=drinks)
+    rendered_page = template.render(years_since_foundation=years_since_foundation,
+                                    year_word=define_year_word(years_since_foundation),
+                                    drinks=drinks)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
